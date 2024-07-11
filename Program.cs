9c5c56fbdbbf601a36f8528ace22042a4cfbd816
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-11 22:30:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, reverse = 0, copy;
+            Console.Write("Enter first number :");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            copy = number1;
+            while (number1 > 0)
+            {
+                reverse = reverse * 10;
+                reverse = reverse + (number1 % 10);
+                number1 = number1 / 10;
+            }
+
+            Console.WriteLine("Reverse of " + copy + " is " + reverse);
+            Console.ReadLine();
+        }
+    }
+}
